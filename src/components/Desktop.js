a960be3f2d4477744ab02d689f9587262d1e84b2
snehakarki <sneha25karki@gmail.com>
2025-02-15 15:11:06 +0530

    desktop icon changes

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -2,13 +2,15 @@ import React from "react";
 import Icon from "./Icon";
 import Taskbar from "./Taskbar";
 import resumeIcon from "../assets/icons/resume_icon.png"; 
+import githubIcon from "../assets/icons/github_icon.png"; 
+import leetcodeIcon from "../assets/icons/leetcode_icon.png"; 
 import "../styles/Desktop.css";
 
 const Desktop = () => {
   const icons = [
     { id: 1, name: "Resume", icon: resumeIcon }, 
-    { id: 2, name: "Projects", icon: resumeIcon }, 
-    { id: 3, name: "Contact", icon: resumeIcon }
+    { id: 2, name: "GitHub", icon: githubIcon}, 
+    { id: 3, name: "Leetcode", icon: leetcodeIcon }
 ];
 
 
