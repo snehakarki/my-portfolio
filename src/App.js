1584b05515a9a348115820786a35841a8e356e14
snehakarki <sneha25karki@gmail.com>
2025-02-15 13:01:22 +0530

    Resolved merge conflicts in App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
 import Desktop from "./components/Desktop";
 
 function App() {
   return <Desktop />;
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> 7d4a7b8 (Initialize project using Create React App)
-=======
-import React from "react";
-import Desktop from "./components/Desktop";
-
-function App() {
-  return <Desktop />;
->>>>>>> 440ba9d (initial commit)
 }
-
 export default App;
