de9101f17058bb014a1b903fc7e7cdcfec552c9c
snehakarki <sneha25karki@gmail.com>
2025-02-15 15:23:49 +0530

    Taskbar css updation

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,33 +1,27 @@
 import React from "react";
-import { FaWindows, FaSearch, FaCloud } from "react-icons/fa";
+import { FaWindows } from "react-icons/fa";
 import "../styles/Taskbar.css";
 
 const Taskbar = () => {
-  const currentTime = new Date();
-  const time = currentTime.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-  const date = currentTime.toLocaleDateString([], { weekday: "short", day: "2-digit", month: "short" });
-
   return (
     <div className="taskbar">
-      {/* Left Section - Weather Info */}
-      <div className="taskbar-left">
-        <FaCloud className="weather-icon" />
-        <span>Cloudy, 20°C</span>
+      {/* Left Section - Weather */}
+      <div className="left-section">
+        <span>☁️ 20°C</span>
       </div>
 
-      {/* Middle Section - Windows Button, Search Bar, and Pinned Apps */}
-      <div className="taskbar-middle">
-        <button className="start-btn"><FaWindows /></button>
-        <div className="search-bar">
-          <FaSearch className="search-icon" />
-          <input type="text" placeholder="Search..." />
-        </div>
+      {/* Center Section - Start Button & Search */}
+      <div className="center-section">
+        <button className="start-btn">
+          <FaWindows size={20} />
+        </button>
+        <input type="text" className="search-bar" placeholder="Search..." />
       </div>
 
       {/* Right Section - Date & Time */}
-      <div className="taskbar-right">
-        <div className="date">{date}</div>
-        <div className="time">{time}</div>
+      <div className="right-section">
+        <span>{new Date().toLocaleDateString()}</span>
+        <span>{new Date().toLocaleTimeString()}</span>
       </div>
     </div>
   );
