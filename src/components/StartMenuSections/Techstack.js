586992ec5e982e229491e06059bff9dcf7672126
snehakarki <sneha25karki@gmail.com>
2025-02-23 19:57:47 +0530

    Techstack section added

--- src/components/StartMenuSections/Techstack.js
+++ src/components/StartMenuSections/Techstack.js
@@ -1,15 +1,27 @@
 import React from "react";
-import "../../styles/StartMenu.css"; 
+import "../../styles/Techstack.css"; 
+
+const techStack = [
+  { name: "Flutter", file: "flutter.png" },
+  { name: "Dart", file: "dart.png" },
+  { name: "Java", file: "java.png" },
+  { name: "Android Development", file: "android.png" },
+  { name: "Firebase", file: "firebase.png" },
+  { name: "MySQL", file: "mysql.png" },
+  { name: "Redis", file: "redis.png" },
+  { name: "React.js", file: "reactjs.png" }
+];
 
 const TechStack = () => {
-  const techStack = ["flutter", "dart", "java", "android", "firebase", "mysql", "redis", "reactjs"];
-
   return (
     <div className="tech-stack">
       <h2>Tech Stack</h2>
-      <div className="tech-icons">
+      <div className="tech-container">
         {techStack.map((tech) => (
-          <img key={tech} src={`/assets/${tech}.png`} alt={tech} className="tech-icon" />
+          <div key={tech.name} className="tech-box">
+            <img src={`/assets/${tech.file}`} alt={tech.name} className="tech-icon" />
+            <p className="tech-name">{tech.name}</p>
+          </div>
         ))}
       </div>
     </div>
