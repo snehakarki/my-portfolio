8c08d4d66be085a9e31b65752a21c3f22835ce7a
snehakarki <sneha25karki@gmail.com>
2025-03-09 13:25:24 +0530

    Mobile Component and hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import Popup from "./components/Popup"; 
-import Desktop from "./components/Desktop"; 
+import Popup from "./components/Desktop/Popup";
+import Desktop from "./components/Desktop/Desktop";
+import Mobile from "./components/Mobile/Home";
+import useIsMobile from "./hooks/useIsMobile"; 
 
 const App = () => {
-  const [showPopup, setShowPopup] = useState(true); 
+  const [showPopup, setShowPopup] = useState(true);
+  const isMobile = useIsMobile();
 
   return (
     <div>
       {showPopup && <Popup onClose={() => setShowPopup(false)} />}
-      <Desktop />
+      {isMobile ? <Mobile /> : <Desktop />}
     </div>
   );
 };
