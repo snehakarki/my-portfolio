ef40ca9d7ca165bbd8beac1a7d93d8dfab2a2245
snehakarki <sneha25karki@gmail.com>
2025-04-15 00:16:38 +0530

    Add app icons and update AppGrid styling

--- src/components/Mobile/AppGrid.js
+++ src/components/Mobile/AppGrid.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../../styles/Mobile/AppGrid.css";
+import linkedinIcon from "../../assets/icons/linkedin_icon.png";
+import githubIcon from "../../assets/icons/github_icon.png";
+import pdfIcon from "../../assets/icons/pdf_icon.png";
+import mailicon from "../../assets/icons/gmail_icon.png";
 
 const apps = [
-  { id: 1, name: "Phone", icon: "📞" },
-  { id: 2, name: "WhatsApp", icon: "💬" },
-  { id: 3, name: "Gallery", icon: "🖼️" },
-  { id: 4, name: "Camera", icon: "📷" },
+  { id: 1, name: "LinkedIn", icon: linkedinIcon },
+  { id: 2, name: "GitHub", icon: githubIcon },
+  { id: 3, name: "Resume", icon: pdfIcon },
+  { id: 4, name: "Email", icon: mailicon },
 ];
 
 const AppGrid = () => {
@@ -13,7 +17,7 @@ const AppGrid = () => {
     <div className="app-grid">
       {apps.map((app) => (
         <div key={app.id} className="app-icon">
-          <span>{app.icon}</span>
+          <img src={app.icon} alt={app.name} />
           <p>{app.name}</p>
         </div>
       ))}
